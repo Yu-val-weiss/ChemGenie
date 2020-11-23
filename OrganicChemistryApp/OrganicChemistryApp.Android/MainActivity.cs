6a281fbd86c9a6ca41b77f0495f37d633ada5d43
Yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2020-11-23 19:57:42 +0000

    Beginning to create the UI of the app
    - Experimenting with Skia
    - Experimenting with Xamarin

--- OrganicChemistryApp/OrganicChemistryApp.Android/MainActivity.cs
+++ OrganicChemistryApp/OrganicChemistryApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace OrganicChemistryApp.Droid
 {
-    [Activity(Label = "OrganicChemistryApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ChemGenie", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ OrganicChemistryApp/OrganicChemistryApp/Views/SkiaCanvas.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrganicChemistryApp.Views
+{
+    public partial class SkiaCanvas : ContentPage
+    { 
+        
+        bool showFill = true;
+
+        public SkiaCanvas()
+        {
+            InitializeComponent();
+        }
+
+
+        void OnCanvasViewTapped(object sender, EventArgs args)
+        {
+            
+            showFill ^= true;
+            (sender as SKCanvasView).InvalidateSurface();
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.DarkMagenta.ToSKColor(),
+                StrokeWidth = 50
+            };
+            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+
+            if (showFill)
+            {
+                paint.Style = SKPaintStyle.Fill;
+                paint.Color = SKColors.DarkOliveGreen;
+                canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
+            }
+        }
+    }
+
+}
